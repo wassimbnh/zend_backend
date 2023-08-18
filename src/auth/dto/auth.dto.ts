03948d45ffd54987c71d03242be8d915b8afb0ff
wassimbnh <104159727+wassimbnh@users.noreply.github.com>
2023-08-18 12:17:37 +0100

    swagger config

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,11 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
 import {IsNotEmpty, IsString} from "class-validator";
 
 export class AuthDto {
 
+    @ApiProperty({
+        description:"Email",
+        example:"wassim.benhedia@esprit.tn"
+    })
     @IsNotEmpty()
     @IsString()
     email: string;
     
+    @ApiProperty({
+        description:"Password",
+    })
     @IsNotEmpty()
     @IsString()
     password: string;
